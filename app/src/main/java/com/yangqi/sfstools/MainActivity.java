f6781e4dd0ebf6254f64ee95f9142e5bfebd5eac
YANGQI <1792443798@qq.com>
2023-12-22 22:55:36 +0800

    完成主界面布局，添加About界面

--- app/src/main/java/com/yangqi/sfstools/MainActivity.java
+++ app/src/main/java/com/yangqi/sfstools/MainActivity.java
@@ -1,8 +1,10 @@
 package com.yangqi.sfstools;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("SFS ToolBox");
+    }
+
+    public void About(View view) {
+        Intent intent = new Intent(this,AboutActivity.class);
+        startActivity(intent);
+    }
+
+    public void DownloadSFS(View view) {
+        Toast.makeText(MainActivity.this, "正在下载SFS...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void StartSFS(View view) {
+        Toast.makeText(MainActivity.this, "正在启动SFS...C", Toast.LENGTH_SHORT).show();
     }
 }
