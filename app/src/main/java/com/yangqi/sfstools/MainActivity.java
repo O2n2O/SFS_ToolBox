37865f0457bdb764d49c594a640ba16fa72d0508
YANGQI <1792443798@qq.com>
2023-12-23 14:14:29 +0800

    在OnCreate方法中写了关于下载文件的代码，但是不知道为什么未正常下载《弃坑！》

--- app/src/main/java/com/yangqi/sfstools/MainActivity.java
+++ app/src/main/java/com/yangqi/sfstools/MainActivity.java
@@ -1,9 +1,13 @@
 package com.yangqi.sfstools;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("SFS ToolBox");
+        new Thread("dsfsdata") {
+            // 步骤2：复写run（），内容 = 定义线程行为
+            @Override
+            public void run() {
+                // 步骤3：通过 线程对象 控制线程的状态，如 运行、睡眠、挂起  / 停止
+                //下载路径，如果路径无效了，可换成你的下载路径
+                String url = "https://gitee.com/yangqicn/sfs-toolbox-data/raw/master/Data/sfsdata.txt";
+                //创建下载任务,downloadUrl就是下载链接
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                //指定下载路径和下载文件名
+                request.setDestinationInExternalPublicDir("/sdcard/Download/SFSTOOLBOX/data.txt", url.substring(url.lastIndexOf("/") + 1));
+                //获取下载管理器
+                DownloadManager downloadManager= (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                //将下载任务加入下载队列，否则不会进行下载
+                downloadManager.enqueue(request);
+            }
+
+            public void start() {
+
+            }
+        };
     }
 
     public void About(View view) {
@@ -27,4 +52,5 @@ public class MainActivity extends AppCompatActivity {
     public void StartSFS(View view) {
         Toast.makeText(MainActivity.this, "正在启动SFS...", Toast.LENGTH_SHORT).show();
     }
+
 }
